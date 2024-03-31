135b2dd3c5aee34d6e9811e0036a4567ed15af93
DigiReform Consult <86280872+Digireform@users.noreply.github.com>
2024-03-31 21:12:16 +0100

    Updated files

--- users-add.php
+++ users-add.php
@@ -6,6 +6,8 @@
 	$_SESSION['table'] = 'users';
 	$user = $_SESSION['user'];
 
+	$users = include('database/show.php')
+
 
 ?>
 
@@ -32,42 +34,101 @@
               <?php include('partials/app-topnav.php') ?>       
 
 		    <div class="dashboard_content">
+			    <div class="dashboard_content_main">
 
-            
+                    <div class="row">
+				        <div class="column column-5">
+						    <h1 class="section_header"><i class="fa fa-plus"></i> Register User</h1>
 
-				<div class="dashboard_content_main">
-                <form action="database/add.php" method="POST" class="appForm">
-					<div class="appFormInputContainer">
-						<label for="first_name">First Name</label>
-						<input type="text" class="appFormInput" id="first_name" name="first_name" />	
-					</div>
-					<div class="appFormInputContainer">
-						<label for="last_name">Last Name</label>
-						<input type="text" class="appFormInput" id="last_name" name="last_name" />	
-					</div>
-					<div class="appFormInputContainer">
-						<label for="email">Email</label>
-						<input type="text" class="appFormInput" id="email" name="email" />	
-					</div>
-					<div class="appFormInputContainer">
-						<label for="password">Password</label>
-						<input type="password" class="appFormInput" id="password" name="password" />	
-					</div>
-					<button type="submit" class="appBtn"><i class="fa fa-plus"></i> Add User</button>
-				 </form>	
-                 <?php 
-					if(isset($_SESSION['response'])){
-						$response_message = $_SESSION['response']['message'];
-						$is_success = $_SESSION['response']['success'];
-				?>
-					<div class="responseMessage">
-				    	<p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>" >
-											<?= $response_message ?>
-						</p>
-				</div>
-						<?php unset($_SESSION['response']); }  ?>
+
+
+
+                            <form action="database/add.php" method="POST" class="appForm">
+					            <div class="appFormInputContainer">
+						            <label for="first_name">First Name</label>
+						            <input type="text" class="appFormInput" id="first_name" name="first_name" />	
+					            </div>
+					            <div class="appFormInputContainer">
+						            <label for="last_name">Last Name</label>
+						            <input type="text" class="appFormInput" id="last_name" name="last_name" />	
+					            </div>
+					            <div class="appFormInputContainer">
+						            <label for="email">Email</label>
+						            <input type="text" class="appFormInput" id="email" name="email" />	
+					            </div>
+					            <div class="appFormInputContainer">
+						            <label for="password">Password</label>
+						            <input type="password" class="appFormInput" id="password" name="password" />	
+					            </div>
+					            <button type="submit" class="appBtn"><i class="fa fa-plus"></i> Add User</button>
+				           </form>	
+                            <?php 
+					            if(isset($_SESSION['response'])){												   
+					    	        $response_message = $_SESSION['response']['message'];
+						            $is_success = $_SESSION['response']['success'];
+				            ?>
+					            <div class="responseMessage">
+				    	            <p class="responseMessage <?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>" >
+										<?= $response_message ?>
+						            </p>
+				                </div>
+						    <?php unset($_SESSION['response']); }  ?>
 		
-				</div>					
+				        	
+
+
+
+
+					    </div>
+
+				        <div class="column column-7">
+						<h1 class="section_header"><i class="fa fa-list"></i> List of User</h1>
+						<div class="section_content">
+							<div class="users">
+								<table>
+									<thead>
+									<tr>
+									    <th>#</th>
+										<th>First Name</th>
+									 	<th>Last Name</th>
+										<th>Email</th>
+										<th>Created At</th>
+										<th>Updated At</th>
+									</tr>
+
+									</thead>
+									<tbody>
+										<?php foreach($users as $index => $user){ ?>
+									        <tr>
+										        <td><?= $index + 1 ?></td>
+										        <td><?= $user['first_name'] ?></td>
+									 	        <td><?= $user['last_name'] ?></td>
+										        <td><?= $user['email'] ?></td>
+										        <td><?= date('M d,Y @ h:i:s A', strtotime($user['created_at'])) ?></td>
+												<td><?= date('M d,Y @ h:i:s A', strtotime($user['updated_at'])) ?></td>
+									        </tr>
+                                        <?php } ?>
+									</tbody>
+
+								</table>
+								<p class="userCount"><?= count($users) ?> Users </P>
+
+							</div>
+
+						</div>
+
+
+						</div>
+				   </div>
+
+                   
+
+					
+
+
+                </div>
+
+
 			</div>
 		</div>
 	</div>
