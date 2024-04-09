3595457e8d91cb336eaf81e151b5d2649f16ae98
DigiReform Consult <86280872+Digireform@users.noreply.github.com>
2024-04-09 19:25:32 +0100

    Updated app-sidebar.php
    Updated file to add more menus to the system

--- partials/app-sidebar.php
+++ partials/app-sidebar.php
@@ -5,15 +5,46 @@
 		<span><?= $user['first_name'] . ' ' . $user['last_name'] ?></span>
 	</div>
   
-         <div class="dashboard_sidebar_menus">
+            <div class="dashboard_sidebar_menus">
                <ul class="dashboard_menu_lists">
                    <!-- class="menuActive"  -->
-                    <li>
+                    <li class="liMainMenu">
                         <a href="./dashboard.php"><i class="fa fa-dashboard"></i> <span class="menuText">Dashboard</span></a>
                     </li>			
-                    <li>
-                    <a href="./users-add.php"> <i class="fa fa-user-plus"></i> <span class="menuText">Add User</span></a>
+                    <li class="liMainMenu">
+                    <a href="javascript:void(0);" class="showHideSubMenu" > 
+                            <i class="fa fa-tag showHideSubMenu" ></i>
+                            <span class="menuText showHideSubMenu" >Products</span>
+                            <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenuw" ></i>
+                        </a>
+                        <ul class="subMenus">
+					        <li><a class="subMenuLink" href="#"> <i class="fa fa-circle-o"></i> View Product</a></li>
+					        <li><a class="subMenuLink" href="#"> <i class="fa fa-circle-o"></i> Add Product</a></li>
+				        </ul>					
+                    </li>
+                    <li class="liMainMenu">
+                    
+                        <a href="javascript:void(0);" class="showHideSubMenu" > 
+                            <i class="fa fa-truck showHideSubMenu" ></i>
+                            <span class="menuText showHideSubMenu" >Supplier</span>
+                            <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenuw" ></i>
+                        </a>
+                        <ul class="subMenus">
+					        <li><a class="subMenuLink" href="#"> <i class="fa fa-circle-o"></i> View Suppler</a></li>
+					        <li><a class="subMenuLink" href="#"> <i class="fa fa-circle-o"></i> Add Supplier</a></li>
+				        </ul>					
                     </li>			
-               </ul>
-         </div>
+                    <li class="liMainMenu showHideSubMenu" >
+                        <a href="javascript:void(0);" class="showHideSubMenu" > 
+                            <i class="fa fa-user-plus showHideSubMenu" ></i>
+                            <span class="menuText showHideSubMenu" >User</span>
+                            <i class="fa fa-angle-left mainMenuIconArrow showHideSubMenuw" ></i>
+                        </a>
+                        <ul class="subMenus">
+					        <li><a class="subMenuLink" href="#"> <i class="fa fa-circle-o"></i> View Users</a></li>
+					        <li><a class="subMenuLink" href="./users-add.php"> <i class="fa fa-circle-o"></i> Add User</a></li>
+				        </ul>					
+                    </li>
+                </ui>
+            </div>
 </div>
