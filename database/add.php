bb2e3095098c5286ff9bd2e95d918cf0c9eaafae
DigiReform Consult <86280872+Digireform@users.noreply.github.com>
2024-04-23 20:14:36 +0100

    Updated add.php
    Updated file to integrate product feature

--- database/add.php
+++ database/add.php
@@ -1,28 +1,51 @@
 <?php
 	// Start the session.
 	session_start();
+	// Capture the table mappings.
+	include('table_columns.php');
 
-    $table_name = $_SESSION['table'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $encrypted = password_hash($password, PASSWORD_DEFAULT);
+	// Capture the table name.
+	$table_name = $_SESSION['table'];
+	$columns = $table_columns_mapping[$table_name];
+
+
+ 
+
+	// Loop through the columns
+	$db_arr = [];
+	$user = $_SESSION['user'];
+	foreach($columns as $column){
+		if(in_array($column, ['created_at', 'updated_at'])) $value = date('Y-m-d H:i:s');
+		else if ($column == 'created_by') $value = $user['id'];
+        else if ($column == 'password') $value = password_hash($_POST[$column], PASSWORD_DEFAULT);
+		else $value = isset($_POST[$column]) ? $_POST[$column] : '';
+        
+		$db_arr[$column] = $value;
+	}
+
+    $table_properties = implode(", ",   array_keys($db_arr));
+	$table_placeholders = ':' . implode(", :", array_keys($db_arr));
+ 
 
    
-    //Adding the record
-    try {
-        $command = "INSERT INTO 
-                                $table_name(first_name,  last_name, email, password, created_at, updated_at)
-                           VALUES
-                               ('".$first_name."', '".$last_name."', '".$email."', '".$encrypted."', NOW(), NOW())";   
 
-        include('connection.php');
+	// Adding the record to main table.
+	try {			
+		$sql = "INSERT INTO 
+								$table_name($table_properties) 
+							VALUES 
+								($table_placeholders)";
 
-        $conn->exec($command);
+		include('connection.php');
+
+		$stmt = $conn->prepare($sql);
+		$stmt->execute($db_arr);
+
+
+
 		$response = [
 			'success' => true,
-			'message' => $first_name . ' ' . $last_name . ' Successfully added to the system. '
+			'message' =>  ' Successfully added to the system. '
 		];
 	} catch (PDOException $e) {
 		$response = [
@@ -32,6 +55,6 @@
 	}
 
 	$_SESSION['response'] = $response;
-	header('location: ../users-add.php')
+	header('location: ../'  . $_SESSION['redirect_to']);
 
 ?>
