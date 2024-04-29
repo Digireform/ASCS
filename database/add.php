cdedb51d9e7e53fb3a34cae58d575c6ab1ca7427
DigiReform Consult <86280872+Digireform@users.noreply.github.com>
2024-04-29 06:15:46 +0100

    Updated add.php

--- database/add.php
+++ database/add.php
@@ -18,39 +18,59 @@
 		if(in_array($column, ['created_at', 'updated_at'])) $value = date('Y-m-d H:i:s');
 		else if ($column == 'created_by') $value = $user['id'];
         else if ($column == 'password') $value = password_hash($_POST[$column], PASSWORD_DEFAULT);
-		
 		else if ($column == 'img') {
 			// Upload or move the file to our directory
 			$target_dir = "../uploads/products/";
 			$file_data = $_FILES[$column];
-			$file_name = $file_data['name'];
-			$file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
-
-			$file_name = 'product-'  . time() .'.' . $file_ext;
 
 
 
+			$value = NULL;
+			$file_data = $_FILES['img'];
 
 
-			$check = getimagesize($file_data['tmp_name']);
 
+
+
+			if($file_data['tmp_name'] !== ''){	
+				$file_name = $file_data['name'];
+				$file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
+				$file_name = 'product-' . time()  . '.' . $file_ext;
+
+
+				$check = getimagesize($file_data['tmp_name']);
 				// Move the file
-			if($check){
-				if(move_uploaded_file($file_data['tmp_name'], $target_dir . $file_name)){
-					// Save the file_name to the database. 
+				if($check){
+					if(move_uploaded_file($file_data['tmp_name'], $target_dir . $file_name)){
+						// Save the file_name to the database. 
 						$value = $file_name;
-			    }
-			} else {
-					//Don't move the file
+					}
+				}
 			}
 		}
 		else $value = isset($_POST[$column]) ? $_POST[$column] : '';
-        
 		$db_arr[$column] = $value;
 	}
 
+
     $table_properties = implode(", ",   array_keys($db_arr));
 	$table_placeholders = ':' . implode(", :", array_keys($db_arr));
+
+
+
+	// Add error handler if permissions is empty.
+	if(isset($db_arr['permissions'])){
+		if($db_arr['permissions'] == ''){
+			$_SESSION['response'] = [
+				'success' => false,
+				'message' => 'Please make sure permission is set!'
+			];
+
+			header('location: ../'  . $_SESSION['redirect_to']);
+			die;
+		}
+	}
+
  
 
    
@@ -66,6 +86,37 @@
 
 		$stmt = $conn->prepare($sql);
 		$stmt->execute($db_arr);
+
+
+
+		// Get saved id
+		$product_id = $conn->lastInsertId();
+
+
+		// Add supplier
+		if($table_name === 'products'){
+			$suppliers = isset($_POST['suppliers']) ? $_POST['suppliers'] : [];
+			if($suppliers){
+				// Loop through the suppliers and add record
+				foreach($suppliers as $supplier){
+					$supplier_data = [
+						'supplier_id' => $supplier,
+						'product_id' => $product_id,
+						'updated_at' => date('Y-m-d H:i:s'),
+						'created_at' => date('Y-m-d H:i:s')
+					];
+
+
+					$sql = "INSERT INTO productsuppliers			
+								(supplier, product, updated_at, created_at) 
+							VALUES 
+								(:supplier_id, :product_id, :updated_at, :created_at)";
+					$stmt = $conn->prepare($sql);
+					$stmt->execute($supplier_data);
+				}
+			}
+		}
+
 
 
 
